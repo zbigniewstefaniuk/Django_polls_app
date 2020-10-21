fcfdb58daaad8eeb8e2835ecec9672be37e487d7
zbigniewstefaniuk <zbyszek791@hotmail.com>
2020-10-21 21:06:39 +0200

    added Question change option in admin panel

--- clasic_app/polls/admin.py
+++ clasic_app/polls/admin.py
@@ -3,12 +3,20 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+class ChoiceInline(admin.TabularInline):
+    # basically it says to Django : Choice Obj are edited on admin site, render 3 choices forms
+    model = Choice
+    extra = 3
+
+
 # allowing in admin panel to edit/add question
 class QuestionAdmin(admin.ModelAdmin):
     fieldssets = [
-        (None, {'fields': ['pub_date', 'question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': [
+         'pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
