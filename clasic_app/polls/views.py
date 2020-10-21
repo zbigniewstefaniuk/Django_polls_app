4404db25d10b4fce6bbd30d99a5f65e52a9cf5a1
zbigniewstefaniuk <zbyszek791@hotmail.com>
2020-10-22 00:37:19 +0200

    bug fix - danger string improvement

--- clasic_app/polls/views.py
+++ clasic_app/polls/views.py
@@ -43,7 +43,7 @@ def vote(request, question_id):
         # redisplaying the question voting form
         return render(request, 'polls/detail.html', {
             'question': question,
-            'error_message': 'You didnt select a choice'
+            'error_message': 'You didnt select a choice!'
         })
     else:
         selected_choice.votes += 1
